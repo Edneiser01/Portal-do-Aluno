d9aabf7f9d9c839d7d2e906fb4d56f67f9fdcb24
Edneiser Nogueira <edneisern01@gmail.com>
2024-04-01 16:04:53 +0100

    Adicionei um botão animado de exemplo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <title>Portal do Aluno</title>
 </head>
 <body>
+   
     <a href="/"> Página Principal</a>
     <a href="/aluno"> Página do Aluno</a>
     <a href="/professor"> Página do professor</a>
